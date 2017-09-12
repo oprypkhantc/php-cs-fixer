d56c8b5205f524daa0912778bb917a46876a6524
Aerendir <hello@aerendir.me>
2017-09-12 11:14:15 +0200

    Fix #2996 and Fix #3045: Add a `Compare` command to show existing built-in fixers and the ones that are actually configured to be used.

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 namespace PhpCsFixer\Console;
 
+use PhpCsFixer\Console\Command\CompareCommand;
 use PhpCsFixer\Console\Command\DescribeCommand;
 use PhpCsFixer\Console\Command\FixCommand;
 use PhpCsFixer\Console\Command\HelpCommand;
@@ -51,6 +52,7 @@ final class Application extends BaseApplication
         $this->toolInfo = new ToolInfo();
 
         // in alphabetical order
+        $this->add(new CompareCommand());
         $this->add(new DescribeCommand());
         $this->add(new FixCommand($this->toolInfo));
         $this->add(new ListFilesCommand($this->toolInfo));
